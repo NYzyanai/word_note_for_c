5aae9af05ee21f72003c310ee2662681c2ffe206
NY <62198538+NYzyanai@users.noreply.github.com>
2021-08-18 23:05:09 +0900

    多分

---
+++ all_word.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charaset="UTF-8">
+    <title>Word_note</title>
+    <link rel="stylesheet" href="./main.css" />
+    <link rel="icon" href="./favicon.ico" id="favicon">
+</head>
+
+
+<body>
+    <header class="header">
+        <div class="main_title">
+            <a href='https://word-note.main.jp/'>
+                <p>
+                    自分の、
+                    <br>
+                    自分による、
+                    <br>
+                    自分のための単語帳
+                </p>
+            </a>
+        </div>
+
+        <div class="sub_title">
+            <form method=post action='https://word-note.main.jp/settings.php'><input type='hidden' name='settings' value='total_settings'><button class='settings_button'><img src='/img/iconmonstr-gear-6-32.png'></button></form>
+        </div>
+    </header>
+
+    <div class='all_wrap_card'>
+        <div class='all_button'>
+            <p>ボタン</p>
+        </div>
+        <div class='all_card'>
+            <h3>Question</h3>
+            <h4>Answer</h4>
+        </div>
+        <div class='clear'></div>
+    </div>
+
+
+    <div class='all_wrap_card'>
+        <div class='all_button'>
+            <p>ボタン</p>
+        </div>
+        <div class='all_card'>
+            <h3>Question</h3>
+            <h4>Answer</h4>
+        </div>
+        <div class='clear'></div>
+    </div>
+
+
+    <div class='all_wrap_card'>
+        <div class='all_button'>
+            <p>ボタン</p>
+        </div>
+        <div class='all_card'>
+            <h3>Question</h3>
+            <h4>Answer</h4>
+        </div>
+        <div class='clear'></div>
+    </div>
+
+
+
+</body>
+
+</html>
